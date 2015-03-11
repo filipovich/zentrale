afef4d4135d8bbfdfc2b5c1da92ec36382e62e09
ivanbenko <bivans209@gmail.com>
2015-03-11 10:37:58 +0100

    yet another changes

--- setup_base_packages_by_deployer.sh
+++ setup_base_packages_by_deployer.sh
@@ -6,13 +6,17 @@ sudo aptitude install -y \
   sqlite3 libsqlite3-dev \
   libxslt-dev libxml2-dev \
   curl wget git-core imagemagick \
-  mc
+  mc \
+  libpq-dev \
+  nodejs postgresql postgresql-contrib \
+  postgres-xc
 
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone git://github.com/dcarley/rbenv-sudo.git ~/.rbenv/plugins/rbenv-sudo
 
 source ~/.bashrc
 
@@ -34,6 +38,16 @@ sudo add-apt-repository ppa:nginx/stable
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
+# NOTE www-data user hasn't rigths to /var/www/apps folder (need for serving static assets)
+
 sudo mkdir -p /var/www/apps
 sudo chown -R deployer /var/www/apps
 sudo chmod -R 0766 /var/www/apps
+
+# install ruby version
+# install bundler
+
+# create role deployer;
+# 
+# postgresql roles
+# https://www.digitalocean.com/community/tutorials/how-to-use-roles-and-manage-grant-permissions-in-postgresql-on-a-vps--2
